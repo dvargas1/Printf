af2e56909fc92e7bd54cceeb089aac39e025376d
dvargas1 <daniel_v93@hotmail.com>
2022-06-25 11:24:48 -0300

    Main Update

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: dvargas <dvarags@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/18 17:24:18 by dvargas           #+#    #+#             */
-/*   Updated: 2022/06/22 01:30:45 by dvargas          ###   ########.fr       */
+/*   Updated: 2022/06/25 11:24:20 by dvargas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,30 +16,44 @@ int main()
 {
 	ft_printf("LINDO TESTE DA PRINTF \n");
 	char c = 'D';
-	ft_printf("Tentando imprimir caractere: %c \n", c) ;
+	printf("Original tentando imprimir um caractere %c \n", c);
+	ft_printf("FT imprimindo caractere: %c \n", c) ;
 	ft_printf("------------------------------------ \n");
 	char *s = "Luna é Linda";
-	ft_printf("Tentando imprimir uma string %s \n", s) ;
+	printf("Original tentando imprimir uma string %s \n", s);
+	ft_printf("FT imprimindo uma string %s \n", s) ;
 	ft_printf("------------------------------------ \n");
 	char *p = "lindo";
-	ft_printf("Tentando imprimir o ponteiro %p \n", p) ;
-	printf("Original é %p \n", p);
+	printf("Original tentando imprimir o endereço de ponteiro %p \n",p);
+	ft_printf("FT imprimindo o ponteiro %p \n", p) ;
 	ft_printf("------------------------------------ \n");
 	int d = 0;
-	ft_printf("Tentando imprimir decimal %d \n", d) ;
+	printf("Original imprimindo decimal %d \n", d);
+	ft_printf("FT imprimindo decimal %d \n", d) ;
 	ft_printf("------------------------------------ \n");
 	int i = 0;
-	ft_printf("Tentando imprimir inteiro %d  \n", i);
+	printf("Original imprimindo inteiro %d \n", i);
+	ft_printf("FT imprimindo inteiro %d  \n", i);
 	ft_printf("------------------------------------ \n");
 	unsigned int u = 0;
-	ft_printf("Tentando imprimir unsgined nbr %u  \n", u);
+	printf("Original imprimindo unsigned int %u \n", u);
+	ft_printf("FT imprimindo unsgined nbr %u  \n", u);
 	ft_printf("------------------------------------ \n");
 	int x = 0;
-	ft_printf("Tentando imprimir hexadecimal %x \n", x);
+	printf("Original imprimindo hexadecimal %x \n", x);
+	ft_printf("FT imprimindo hexadecimal %x \n", x);
 	ft_printf("------------------------------------ \n");
 	int X = 0;
-	ft_printf("Tentando imprimir HEXADECIMAL %X \n", X);
+	printf("Original imprimindo HEXADECIMAL Maiusculo %X \n", X);
+	ft_printf("FT imprimindo HEXADECIMAL %X \n", X);
 	ft_printf("------------------------------------ \n");
-	ft_printf("Aqui é só Porcento %%");
+	printf("Original usando a regra Porcento %% \n");
+	ft_printf("FT usando a regra Porcento %% \n");
+	ft_printf("==================================== \n");
+	ft_printf("AGORA TESTES NA LOUCURA DA VIDA \n");
+	char *s1 = "";
+	ft_printf("Testando com string vazia %s \n", s1);
+	printf("Testando com string vazia %s \n", s1);
+	
 }
 
